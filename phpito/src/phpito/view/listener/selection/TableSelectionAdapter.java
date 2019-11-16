8f2dd850efed1aeaf23ba3bd2e6684b0120a3250
hxs <hxs@parrot>
2019-11-16 17:23:00 +0100

    fast update

--- phpito/src/phpito/view/listener/selection/TableSelectionAdapter.java
+++ phpito/src/phpito/view/listener/selection/TableSelectionAdapter.java
@@ -14,6 +14,7 @@ import phpito.view.shell.ShellPHPito;
 public class TableSelectionAdapter extends SelectionAdapter {
 	private ShellPHPito shellPHPito;
 
+	/* CONSTRUCT */
 	public TableSelectionAdapter(ShellPHPito shellPHPito) {
 		super();
 		this.shellPHPito = shellPHPito;
