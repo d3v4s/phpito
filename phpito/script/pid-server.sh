42d07ad93e27cf93adbb97fa8c3abf5e66de6c54
hxs <hxs@parrot>
2019-11-21 02:09:19 +0100

    update script

--- phpito/script/pid-server.sh
+++ phpito/script/pid-server.sh
@@ -3,7 +3,8 @@
 ADDRESS=$1
 PORT=$2
 
-#netstat -ltnp | grep -E '.*('$ADDRESS').*LISTEN.*/php.*'
-ss -Hlptn "src = $ADDRESS and sport = $PORT" 
+# netstat -ltnp | grep -E '.*('$ADDRESS').*LISTEN.*/php.*'
+# ss -Hlptn "src = $ADDRESS and sport = $PORT"
+ss -Hlptn | grep $ADDRESS:$PORT
 
 exit 0
