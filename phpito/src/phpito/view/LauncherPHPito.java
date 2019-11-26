bda0a16deabe81e8a915c2012756d4c0e9534606
hxs <hxs@parrot>
2019-11-26 03:32:18 +0100

    update

--- phpito/src/phpito/view/LauncherPHPito.java
+++ phpito/src/phpito/view/LauncherPHPito.java
@@ -6,7 +6,9 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
+import exception.ApplicationException;
 import exception.FileLockException;
+import exception.SystemException;
 import jaswt.core.Jaswt;
 import jun.Jun;
 import phpito.core.PHPitoManager;
@@ -26,6 +28,7 @@ public class LauncherPHPito {
 		for (String arg : args) if (arg.equals("debug")) PHPitoManager.getInstance().getJoggerDebug().setDebug(true);
 
 		PHPitoManager.getInstance().getJoggerDebug().writeLog("DEBUG MODE ON");
+		PHPitoManager.getInstance().getJoggerDebug().writeLog("STARTING PHPITO");
 		
 		LauncherPHPito launcherPHPito = new LauncherPHPito();
 		launcherPHPito.open();
@@ -36,29 +39,37 @@ public class LauncherPHPito {
 	
 	/* method that open PHPito window */
 	public void open() {
-		PHPitoManager.getInstance().getJoggerDebug().writeLog("STARTING PHPITO");
 
 		try {
 			Jun.getInstance().tryLock();
 			PHPitoManager.getInstance().getJoggerDebug().writeLog("PHPITO LOCK");
-		} catch (Exception e) {
+		} catch (ApplicationException e) {
+			/* phpito already running */
 			String msg = "PHPito already running!!!\nForce second instance???";
 			PHPitoManager.getInstance().getJoggerDebug().writeLog("PHPito Already Running!!!");
 			int resp = Jaswt.getInstance().launchMB(new Shell(), SWT.YES | SWT.NO, "CAUTION!!!", msg);
 
-			if (resp == SWT.YES)
+			if (resp == SWT.YES) {
+				/* force second instance */
 				try {
 					Jun.getInstance().forceLock();
 					PHPitoManager.getInstance().getJoggerDebug().writeLog("PHPito - FORCE SECOND INSTANCE!!!");
 				} catch (IOException | FileLockException e1) {
 					Jaswt.getInstance().launchMBError(new Shell(), e1, PHPitoManager.getInstance().getJoggerError());
+					System.exit(-1);
 				}
-			else return;
+			} else return;
+		} catch (IOException | SystemException e) {
+			Jaswt.getInstance().launchMBError(new Shell(), e, PHPitoManager.getInstance().getJoggerError());
+			System.exit(-1);
 		}
+
+		/* open shell */
 		display = Display.getDefault();
 		shellPHPito = new ShellPHPito(display);
 		shellPHPito.open();
-//		shellPHPito.layout();
+
+		/* start read and dispatch */
 		PHPitoManager.getInstance().getJoggerDebug().writeLog("PHPito Display - START READ DISPATCH");
 		while (!shellPHPito.isDisposed()) {
 			try {
